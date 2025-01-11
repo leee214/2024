d9b611e3c0ec9c10bc8e9911b4216a4bbd3cc40b
leee214 <133885062+leee214@users.noreply.github.com>
2025-01-11 18:50:37 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -9,12 +9,19 @@ int main() {
 	cin.tie(NULL);
 	int L;
 	long long res = 0;
+	long long mul = 1;
 	cin >> L;
 	string str;
 	cin >> str;
 	int fl = 0;
 	for (int i = 0; i < L; i++) {
-		res += (str[i] - 96) * pow(31, fl);
+		mul = 1;
+		for (int j = 0; j < fl; j++) {
+			mul *= 31;
+			mul %= 1234567891;
+		}
+		res += ((str[i] - 96) * mul) % 1234567891;
+		res %= 1234567891;
 		fl++;
 	}
 	cout << res << "\n";
