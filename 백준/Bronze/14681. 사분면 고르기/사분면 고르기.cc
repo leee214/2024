617248fa17e46430105b76f9c3a3e1169970daa9
leee214 <133885062+leee214@users.noreply.github.com>
2025-01-20 04:29:40 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int x, y;
+	cin >> x >> y;
+	if (x > 0 && y > 0) {
+		cout << "1\n";
+	}
+	else if (x < 0 && y > 0) {
+		cout << "2\n";
+	}
+	else if (x < 0 && y < 0) {
+		cout << "3\n";
+	}
+	else if (x > 0 && y < 0) {
+		cout << "4\n";
+	}
+	return 0;
+}
